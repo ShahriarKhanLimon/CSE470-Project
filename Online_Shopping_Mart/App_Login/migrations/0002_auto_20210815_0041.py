785d024c8147cdb7163f3e5b1d6c2d5d06d82b5c
Shahriar Khan Limon <shahriarkhan530@gmail.com>
2023-05-01 04:56:51 +0600

    Adding files

---
+++ Online_Shopping_Mart/App_Login/migrations/0002_auto_20210815_0041.py
@@ -0,0 +1,21 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('App_Login', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='user',
+            name='is_active',
+            field=models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active'),
+        ),
+        migrations.AlterField(
+            model_name='user',
+            name='is_staff',
+            field=models.BooleanField(default=False, help_text='Designates whether the user can login this site', verbose_name='staff status'),
+        ),
+    ]
