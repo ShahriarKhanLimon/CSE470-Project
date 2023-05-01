745cc77cbbdec7f732493ea65cb2b83a4573a591
Shahriar Khan Limon <shahriarkhan530@gmail.com>
2023-05-01 15:37:17 +0600

    Changed Product name to show give failure in test file for App_Shop

--- Online_Shopping_Mart/App_Shop/tests.py
+++ Online_Shopping_Mart/App_Shop/tests.py
@@ -4,18 +4,16 @@ from App_Shop.models import Category, Product
 
 # Create your tests here.
 
-#There isn't any method in my view.py file though I have 2 classes for initializing fields. As there is no method, I am not writing any test for views.py file
-
 #Testing models classes
 class CategoryrTest(TestCase):
     def test__str__(self):
-        category=Category(title="Shirt")
-        self.assertEqual(category.__str__(),"Shirt")
+        category=Category(title="T shirt")
+        self.assertEqual(category.__str__(),"T shirt")
 
 class ProductTest(TestCase):
     def test__str__(self):
         category=Category(title="shirt")
         category.save()
-        item = Product(name="Cotton White Casual Shirt", price=1000, category = category)
+        item = Product(name="Black T shirt", price=500, category = category)
         item.save()
-        self.assertEqual(item.__str__(),"Cotton White Casual Shirt")
+        self.assertEqual(item.__str__(),"Black T shirt")
